80d8e527a236877e2a86e208ee4655a650a7df54
Zteng <zh.teng90@gmail.com>
2019-02-27 18:20:31 +0800

    2019-02-27 18:20
    idnex test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,7 +41,7 @@
                     </button>
 
                     <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
+                    <a class="navbar-brand" href="{{ url('/home') }}">
                         Home
                     </a>
 
